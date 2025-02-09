645f4fe002f675a374d954690b3fc89e71f9a873
Oleksandr <oleks.bodnar@gmail.com>
2025-02-09 13:19:33 +0200

    fix pass for connection

--- connectdb.py
+++ connectdb.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
 
 # Встановлюємо параметри підключення до PostgreSQL
-DB_URL = "postgresql://postgres:05071984@localhost:5432/hw06"
+DB_URL = "postgresql://postgres:mysecretpassword@localhost:5432/hw06"
 
 # Створюємо об'єкт engine для підключення до бази даних
 engine = create_engine(DB_URL)
